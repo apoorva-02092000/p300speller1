029bfb1059244f88dd0e4634ba8d36850c82eca3
apoorva-02092000 <73773023+apoorva-02092000@users.noreply.github.com>
2021-07-16 13:41:54 +0530

    new

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express'); 
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.static('javascriptfiles'));
+
+app.set('view engine', 'ejs'); 
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.listen(process.env.PORT || 3000);
+
