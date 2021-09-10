22386d8cc54bdb44d5eed41c16dba4332090d283
apoorva-02092000 <73773023+apoorva-02092000@users.noreply.github.com>
2021-09-10 11:53:39 +0530

    newwwwwwwwwwwwwwwwwwwwwwwwww

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express'); //USING EXPRESS SERVER,THE SERVER USED FOR NODE.JS
+
+//const session=require('express-session');
+const serverles=require('serverless-http');
 const app = express();
-const session=require('express-session');
+const router=express.Router();
+router.get
 
-
+module.exports.handler=serverles(app);
 
 app.use(express.static('public'));
 app.use(express.static('javascriptfiles'));
@@ -17,24 +21,24 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/devanagri', (req, res) => {
+router.get('/devanagri', (req, res) => {
   res.render('devanagri');
 });
 
-app.get('/english', (req, res) => {
+router.get('/english', (req, res) => {
   res.render('english');
 });
+ app.use('/.netlify/functions/api',router);
 
+app.listen(process.env.PORT || 3000);
 
-//app.listen(process.env.PORT || 3000);
-
-app.listen(3001, '0.0.0.0', function() {
-  console.log('Listening to port:  ' + 3001);
-});
+//app.listen(3001, '0.0.0.0', function() {
+ // console.log('Listening to port:  ' + 3001);
+//});
 
 
 
